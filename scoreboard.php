fb5c8a27933bc5aa947876a516c59dfa4598922d
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-10-06 13:53:29 +0200

    page en cours de réalisation

--- scoreboard.php
+++ scoreboard.php
@@ -1,2 +1,18 @@
 <?php
+ 
+ class Scoreboard{
+    private array $scores = [];
+
+    public function addScores(string $player, int $score): void {
+        $this->scores[] = ['player' => $player, 'score' => $score];
+
+    }
+
+    public function getTopScores(int $limit = 10): array {
+        usort($this->score);
+    }
+ }
+ 
+ 
+ 
  ?>
