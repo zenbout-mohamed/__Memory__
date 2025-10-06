ffa9bbb21e920e87fe0bd3e4ae5ad45e90ff56d9
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-10-06 13:43:21 +0200

    fonction du score réalisé

--- player.php
+++ player.php
@@ -1,6 +1,21 @@
 <?php
 
+class Player {
+    private string $name;
+    private array $scores = [];
 
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function addScore(int $score): void {
+        $this->scores[] = $score;
+    }
+
+    public
+
+}
 
 
 ?>
