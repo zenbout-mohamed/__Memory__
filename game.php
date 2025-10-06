3674f20c2f6a2eaba3dbac40e784e0d87d76707a
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-10-06 10:40:17 +0200

    initialisation du tablau représentatif de fruits

--- game.php
+++ game.php
@@ -1,1 +1,23 @@
-<?php ?>
+<?php
+require_once "card.php";
+
+
+class Game {
+    private $arrayCards = [];
+
+
+    public function __construct(int $pairs){
+        if ($pairs < 3 || $pairs > 12) {
+           throw new Exception("Le nombre pair doit etre en 3 et 12");
+
+           $fruits = ["Pomme ","Banane","Raisin","Pastèque","Cerise","Ananas","Kiwi","Abricot","Noix de Coco","Citron","Mangue","Fraise"];
+
+           shuffle($fruits);
+           $chosen = array_slice($fruits, 0, $pairs);
+           
+        }
+    }
+}
+
+
+?>
