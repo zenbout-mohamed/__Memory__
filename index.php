14823c9b2d9b8662e8b4713a53ff1dace0e8c7c5
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-10-06 14:02:48 +0200

    chantier page index.php

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
 <?php
+require_once "game.php";
+require_once "player.php";
+require_once "scoreboard.php";
 
 
-?>
+$game = new Game(4);
+$cards = $game->getCards();
+
+
+$player = new Player("Paul Pitch");
+$scoreboard = new Scoreboard();
+
+$player->addScore(30);
+$scoreboard->addScore($player->getName(), 30);
+
+
+echo "<h1>Plateau de Jeu</h1>";?>
