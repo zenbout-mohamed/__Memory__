37d33778557e580ab0a40a0d6523e146025b7b51
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-10-06 10:49:13 +0200

    page game.php corrigé

--- game.php
+++ game.php
@@ -1,23 +1,32 @@
 <?php
 require_once "card.php";
 
-
 class Game {
-    private $arrayCards = [];
+    private array $arrayCards = [];
 
-
-    public function __construct(int $pairs){
+    public function __construct(int $pairs) {
         if ($pairs < 3 || $pairs > 12) {
-           throw new Exception("Le nombre pair doit etre en 3 et 12");
-
-           $fruits = ["Pomme ","Banane","Raisin","Pastèque","Cerise","Ananas","Kiwi","Abricot","Noix de Coco","Citron","Mangue","Fraise"];
-
-           shuffle($fruits);
-           $chosen = array_slice($fruits, 0, $pairs);
-           
+            throw new Exception("Le nombre de paires doit être entre 3 et 12");
         }
+
+        $fruits = [
+            "Pomme", "Banane", "Raisin", "Pastèque", "Cerise", "Ananas",
+            "Kiwi", "Abricot", "Noix de Coco", "Citron", "Mangue", "Fraise"
+        ];
+
+        shuffle($fruits);
+        $chosen = array_slice($fruits, 0, $pairs);
+
+        foreach ($chosen as $fruit) {
+            $this->arrayCards[] = new Card($fruit);
+            $this->arrayCards[] = new Card($fruit);
+        }
+
+        shuffle($this->arrayCards);
+    }
+
+    public function getCards(): array {
+        return $this->arrayCards;
     }
 }
-
-
 ?>
