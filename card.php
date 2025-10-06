a715c596a3ab3a8e20e7589d759a926e3569ce06
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-10-06 10:27:36 +0200

    page card.php réalisé

--- card.php
+++ card.php
@@ -1,5 +1,6 @@
 <?php
 
+// J'ai utlisé les getters et les setters :
 class Card {
 
     private string $value;
@@ -9,7 +10,23 @@ class Card {
         $this->value = $value;
     }
 
-    
+    public function getValue(): string{
+        return $this->value;
+    }
+
+    public function isRevealed(): bool {
+        return $this->isRevealed;
+    }
+
+    public function reveal():void {
+        $this->isRevealed = true;
+    }
+
+    public function hide(): void {
+        $this->isRevealed = false;
+    }
+
+
    
 
 }
