f962cee21c3d5049252e74c68f9ec08307f93686
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-10-06 10:21:27 +0200

    page card.php en cours de réalisation

---
+++ card.php
@@ -0,0 +1,18 @@
+<?php
+
+class Card {
+
+    private string $value;
+    private bool $isRevealed = false;
+
+    public function __construct(string $value){
+        $this->value = $value;
+    }
+
+    
+   
+
+}
+
+
+?>
