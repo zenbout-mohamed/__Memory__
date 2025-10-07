7d3907b1f3cea3096219ecd1222da6d47d742be7
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-10-07 09:18:23 +0200

    script tailwind mise en place

--- index.php
+++ index.php
@@ -26,11 +26,12 @@ foreach ($scoreboard->getTopScores() as $entry) {
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Jeux-Memory</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
     
