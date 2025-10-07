3980c5df0eafbe59063b6909d13739633ce1a82e
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-10-07 09:15:55 +0200

    réalisation structure html index.php

--- index.php
+++ index.php
@@ -24,3 +24,15 @@ foreach ($scoreboard->getTopScores() as $entry) {
     echo $entry['player'] . " : " . $entry['score'] . " coups<br>";
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
